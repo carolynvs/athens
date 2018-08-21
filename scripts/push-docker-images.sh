7b8fe5281da7e21a73043e1641d6d6edacf1238d
Carolyn Van Slyck <carolyn.vanslyck@microsoft.com>
2018-08-21 17:10:24 -0500

    use the force luke

--- scripts/push-docker-images.sh
+++ scripts/push-docker-images.sh
@@ -6,19 +6,38 @@ set -xeuo pipefail
 # Manually set the registry to a docker registry where you have push access to test this script.
 REGISTRY=${REGISTRY:-gomods/}
 
-VERSION=${VERSION:-$(git describe --tags --abbrev=7 --dirty | echo $TRAVIS_TAG)}
-BRANCH=${BRANCH:-$(git rev-parse --abbrev-ref HEAD | echo $TRAVIS_BRANCH)}
-REPO_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null && pwd )/"
+# Try to use the travis variables before hitting git because travis does shallow clones for performance reasons.
+VERSION=${VERSION:-$TRAVIS_TAG}
+if [[ -z "$VERSION" ]]; then
+    echo "defaulting VERSION using git..."
+    VERSION=`git describe --tags --abbrev=7 --dirty`
+fi
+
+BRANCH=${BRANCH:-$TRAVIS_BRANCH}
+if [[-z "$BRANCH" ]]; then
+    echo "defaulting BRANCH using git..."
+    BRANCH=`git rev-parse --abbrev-ref HEAD`
+    if [[ "$BRANCH" == "HEAD" ]]; then
+        BRANCH=${VERSION} # Make our branch var work like travis_branch
+    fi
+fi
 
 # mutable tag is the docker image tag that we will reuse between pushes, it is not a stable tag like a SHA or version.
 MUTABLE_TAG=${MUTABLE_TAG:-} # defaulted below based on the branch
-if [[ "$MUTABLE_TAG" == "" ]]; then
-    case "BRANCH" in
-        "HEAD") MUTABLE_TAG="latest";; # tagged releases use the latest tag
-        "master") MUTABLE_TAG="canary";; # the master branch use the canary tag
-        *) MUTABLE_TAG=${BRANCH} # branch builds use the name of the branch
-    esac
+if [[ -z "$MUTABLE_TAG" ]]; then
+    # tagged build
+    if [[ "$VERSION" == "$BRANCH" ]]; then
+        MUTABLE_TAG="latest"
+    # master build
+    elif [[ "$BRANCH" == "master" ]]; then
+        MUTABLE_TAG="canary"
+    # branch build
+    else
+        MUTABLE_TAG=${BRANCH}
+    fi
 fi
+
+REPO_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null && pwd )/"
 
 docker build -t ${REGISTRY}proxy:${VERSION} -f ${REPO_DIR}cmd/proxy/Dockerfile ${REPO_DIR}
 docker build -t ${REGISTRY}olympus:${VERSION} -f ${REPO_DIR}cmd/olympus/Dockerfile ${REPO_DIR}
