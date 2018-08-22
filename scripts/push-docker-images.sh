432381e1fe7edd39eeb099b001e7577dae6a1778
Carolyn Van Slyck <carolyn.vanslyck@microsoft.com>
2018-08-22 10:31:10 -0500

    give up on travis variables

--- scripts/push-docker-images.sh
+++ scripts/push-docker-images.sh
@@ -5,28 +5,14 @@ set -xeuo pipefail
 
 # Manually set the registry to a docker registry where you have push access to test this script.
 REGISTRY=${REGISTRY:-gomods/}
-
-# Try to use the travis variables before hitting git because travis does shallow clones for performance reasons.
-VERSION=${VERSION:-$TRAVIS_TAG}
-if [[ -z "$VERSION" ]]; then
-    echo "defaulting VERSION using git..."
-    VERSION=`git describe --tags --abbrev=7 --dirty`
-fi
-
-BRANCH=${BRANCH:-$TRAVIS_BRANCH}
-if [[-z "$BRANCH" ]]; then
-    echo "defaulting BRANCH using git..."
-    BRANCH=`git rev-parse --abbrev-ref HEAD`
-    if [[ "$BRANCH" == "HEAD" ]]; then
-        BRANCH=${VERSION} # Make our branch var work like travis_branch
-    fi
-fi
+VERSION=${VERSION:-$(git describe --tags --abbrev=7 --dirty)}
+BRANCH=${BRANCH:-$(git rev-parse --abbrev-ref HEAD)}
 
 # mutable tag is the docker image tag that we will reuse between pushes, it is not a stable tag like a SHA or version.
 MUTABLE_TAG=${MUTABLE_TAG:-} # defaulted below based on the branch
 if [[ -z "$MUTABLE_TAG" ]]; then
     # tagged build
-    if [[ "$VERSION" == "$BRANCH" ]]; then
+    if [[ "$BRANCH" == "HEAD" ]]; then
         MUTABLE_TAG="latest"
     # master build
     elif [[ "$BRANCH" == "master" ]]; then
